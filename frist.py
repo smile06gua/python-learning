41de34f6f01ece47563bc07f7e010365f1660b0d
smile06gua <100401169+smile06gua@users.noreply.github.com>
2022-09-02 11:34:28 +0800

    Update frist.py

--- frist.py
+++ frist.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Check this amazing web.</h1>'
 
+def user2('/who/<yourname>):
+    return '<h1>Hello {},have s nice day!</h1>'.format(yourname)
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>It is hard to forget someone who gave you so much to remember.I know it is hard to forget who hurt you a lot. Just because you loved before.Hey {}, remember one thing, I will always by your side.</h1>'.format(name)
