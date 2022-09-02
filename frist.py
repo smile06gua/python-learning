5b472e21974bcf297ff3a6ea9ff3104e94ece418
smile06gua <100401169+smile06gua@users.noreply.github.com>
2022-09-03 00:01:46 +0800

    Update frist.py

--- frist.py
+++ frist.py
@@ -1,21 +1,21 @@
-from flask import Flask, redirect
+from flask import Flask,redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Check this amazing web.</h1>'
 
-@app.route('/who/<yourname>')
-def user2(yourname):
-    return '<h1>Hello {},have s nice day!</h1>'.format(yourname)
-
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>It is hard to forget someone who gave you so much to remember.I know it is hard to forget who hurt you a lot. Just because you loved before.Hey {}, remember one thing, I will always by your side.</h1>'.format(name)
+    return '<h1>My hug baby~~~ It is hard to forget someone who gave you so much to remember.I know it is hard to forget who hurt you a lot. Just because you loved before.Hey {}, remember one thing, I will always by your side.</h1>'.format(name)
 
 @app.route('/tosmileig/')
 def myig():
     return redirect("https://www.instagram.com/smilegoofy_orb/")
 
+@app.route('/checkbabysig/')
+def babysig():
+    return redirect("https://www.instagram.com/sjxi_0302/")
+
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
