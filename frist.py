da04f7a1f9c78a53fc5140330c9071ac397a533b
smile06gua <100401169+smile06gua@users.noreply.github.com>
2022-09-02 10:25:12 +0800

    Create frist.py

---
+++ frist.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Check this amazing web.</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>It is hard to forget someone who gave you so much to remember.I know it is hard to forget who hurt you a lot. Just because you loved before.Hey {}, remember one thing, I will always by your side.</h1>'.format(name)
+
+@app.route('/tosmileig/')
+def myig():
+    return redirect("https://www.instagram.com/smilegoofy_orb/")
+
+if __name__ == '__main__':
+    app.run()
